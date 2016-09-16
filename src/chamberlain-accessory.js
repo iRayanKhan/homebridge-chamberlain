0dcf3a52f46f3916f1fcd60ad857928b4a8a5598
Casey Foster <c@sey.me>
2016-09-16 12:46:03 -0500

    Improve reporting

--- src/chamberlain-accessory.js
+++ src/chamberlain-accessory.js
@@ -1,6 +1,13 @@
 const Api = require('./api');
 const instance = require('./instance');
 
+let waitTimeoutId;
+const wait = delay =>
+  new Promise(resolve => {
+    clearTimeout(waitTimeoutId);
+    waitTimeoutId = setTimeout(resolve, delay * 1000);
+  });
+
 module.exports = class {
   constructor(log, {deviceId, name, password, username}) {
     this.log = log;
@@ -37,13 +44,15 @@ module.exports = class {
     service
       .getCharacteristic(Characteristic.TargetDoorState)
       .on('set', this.setState.bind(this));
+
+    this.reportState();
   }
 
   getState(cb) {
     return this.api.getDeviceAttribute({name: 'doorstate'}).then(
       state => {
         state = this.doorstateToHap[state];
-        this.log(`${this.name} state read as ${this.hapToEnglish[state]}`);
+        this.log(`current state is ${this.hapToEnglish[state]}`);
         cb(null, state);
       },
       cb
@@ -51,15 +60,40 @@ module.exports = class {
   }
 
   setState(state, cb) {
+    this.log(`setting target state to ${this.hapToEnglish[state]}`);
     return this.api.setDeviceAttribute({
       name: 'desireddoorstate',
       value: this.hapToDesireddoorstate[state]
     }).then(
       () => {
-        this.log(`${this.name} state set to ${this.hapToEnglish[state]}`);
+        this.log(`target state set to ${this.hapToEnglish[state]}`);
         cb();
+
+        // Immediately report the current state then report the state over the
+        // next 30 seconds;
+        Promise.resolve()
+          .then(() => this.reportState())
+          .then(() => wait(10))
+          .then(() => this.reportState())
+          .then(() => wait(10))
+          .then(() => this.reportState())
+          .then(() => wait(10))
+          .then(() => this.reportState())
+          .catch(er => this.log(er));
       },
       cb
+    );
+  }
+
+  reportState() {
+    const {CurrentDoorState} = instance.homebridge.hap.Characteristic;
+    return new Promise((resolve, reject) =>
+      this.getState((er, state) => {
+        if (er) return reject(er);
+
+        this.service.setCharacteristic(CurrentDoorState, state);
+        resolve();
+      })
     );
   }
 
