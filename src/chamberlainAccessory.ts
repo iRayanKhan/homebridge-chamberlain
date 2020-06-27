bbe34584849deac307590b0976c473dce13fa4c7
Ryan Furness <furenberger@gmail.com>
2020-06-27 10:12:05 -0500

    format

--- src/chamberlainAccessory.ts
+++ src/chamberlainAccessory.ts
@@ -7,9 +7,7 @@ import type {
   CharacteristicGetCallback,
 } from 'homebridge';
 
-import {
-  CharacteristicEventTypes,
-} from 'homebridge';
+import { CharacteristicEventTypes } from 'homebridge';
 
 import ChamberlainService from './chamberlainService';
 import { ChamberlainHomebridgePlatform } from './platform';
@@ -40,76 +38,84 @@ export class ChamberlainAccessory {
 
   private currentDoorState: CurrentDoorState;
 
-  open = async (callback: (status:boolean) => void) => {
+  open = async (callback: (status: boolean) => void) => {
     this.log.debug('Opening the Garage!');
     let result = false;
-    try{
+    try {
       result = await this.chamberlainService.open();
       this.log.debug('Done waiting for - Opening the Garage!');
-    }catch(error){
+    } catch (error) {
       this.log.debug('Error opening garage: ', error);
     }
     callback(result);
-  }
+  };
 
-    close = async (callback: (status:boolean) => void) => {
-      this.log.debug('Closing the Garage!');
-      let result = false;
-      try{
-        result = await this.chamberlainService.close();
-        this.log.debug('Done waiting for - Closing the Garage!');
-      }catch(error){
-        this.log.debug('Error closing garage: ', error);
-      }
-      callback(result);
+  close = async (callback: (status: boolean) => void) => {
+    this.log.debug('Closing the Garage!');
+    let result = false;
+    try {
+      result = await this.chamberlainService.close();
+      this.log.debug('Done waiting for - Closing the Garage!');
+    } catch (error) {
+      this.log.debug('Error closing garage: ', error);
     }
+    callback(result);
+  };
 
-    status= async (callback: (status:string) => void) =>{
-      this.log.debug('Status queried!');
-      let result = 'Closed';
-      try{
-        result = await this.chamberlainService.status();
-        this.log.debug('new result: ', result);
-      }catch(error){
-        this.log.debug('Error checking status: ', error);
-      }
-      callback(result);
+  status = async (callback: (status: string) => void) => {
+    this.log.debug('Status queried!');
+    let result = 'Closed';
+    try {
+      result = await this.chamberlainService.status();
+      this.log.debug('new result: ', result);
+    } catch (error) {
+      this.log.debug('Error checking status: ', error);
     }
+    callback(result);
+  };
 
-
-    constructor(
+  constructor(
     private readonly platform: ChamberlainHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
-    ){
-      this.log = this.platform.log;
+  ) {
+    this.log = this.platform.log;
 
-      const {username, password, deviceId} = accessory.context.device;
-      const garageDoorOpener = this.platform.Service.GarageDoorOpener;
+    const { username, password, deviceId } = accessory.context.device;
+    const garageDoorOpener = this.platform.Service.GarageDoorOpener;
 
-      this.chamberlainService.init(username, password, deviceId, this.log);
+    this.chamberlainService.init(username, password, deviceId, this.log);
 
-      // get the GarageDoorOpener service if it exists, otherwise create a new GarageDoorOpener service
-      this.service = this.accessory.getService(garageDoorOpener)
-    || this.accessory.addService(garageDoorOpener);
+    // get the GarageDoorOpener service if it exists, otherwise create a new GarageDoorOpener service
+    this.service =
+      this.accessory.getService(garageDoorOpener) ||
+      this.accessory.addService(garageDoorOpener);
 
-      // set the service name, this is what is displayed as the default name on the Home app
-      // we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-      this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+    // set the service name, this is what is displayed as the default name on the Home app
+    // we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.service.setCharacteristic(
+      this.platform.Characteristic.Name,
+      accessory.context.device.name,
+    );
 
-      this.service
-        .getCharacteristic(this.platform.Characteristic.CurrentDoorState)
-        .on(CharacteristicEventTypes.GET, this.getCurrentDoorState.bind(this));
+    this.service
+      .getCharacteristic(this.platform.Characteristic.CurrentDoorState)
+      .on(CharacteristicEventTypes.GET, this.getCurrentDoorState.bind(this));
 
-      this.service
-        .getCharacteristic(this.platform.Characteristic.TargetDoorState)
-        .on(CharacteristicEventTypes.SET, this.setTargetDoorState.bind(this));
+    this.service
+      .getCharacteristic(this.platform.Characteristic.TargetDoorState)
+      .on(CharacteristicEventTypes.SET, this.setTargetDoorState.bind(this));
 
-      this.currentDoorState = CurrentDoorState.CLOSED;
-      this.service.updateCharacteristic(this.platform.Characteristic.CurrentDoorState, this.currentDoorState);
-    }
+    this.currentDoorState = CurrentDoorState.CLOSED;
+    this.service.updateCharacteristic(
+      this.platform.Characteristic.CurrentDoorState,
+      this.currentDoorState,
+    );
+  }
 
   readonly setCurrentDoorState = (state: CurrentDoorState) => {
-    this.platform.log.debug('setting current door state to ' + CurrentDoorState[state]);
+    this.platform.log.debug(
+      'setting current door state to ' + CurrentDoorState[state],
+    );
     this.currentDoorState = state;
     this.service
       .getCharacteristic(this.platform.Characteristic.CurrentDoorState)
@@ -120,10 +126,16 @@ export class ChamberlainAccessory {
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
-  setTargetDoorState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+  setTargetDoorState(
+    value: CharacteristicValue,
+    callback: CharacteristicSetCallback,
+  ) {
     if (value === this.platform.Characteristic.TargetDoorState.CLOSED) {
       const closedCallback = () => {
-        this.service.setCharacteristic(this.platform.Characteristic.CurrentDoorState, this.platform.Characteristic.CurrentDoorState.CLOSED);
+        this.service.setCharacteristic(
+          this.platform.Characteristic.CurrentDoorState,
+          this.platform.Characteristic.CurrentDoorState.CLOSED,
+        );
         this.platform.log.debug('callback close');
         callback();
         this.platform.log.debug('callback close done');
@@ -131,14 +143,15 @@ export class ChamberlainAccessory {
 
       this.close(closedCallback);
       this.platform.log.debug('done setTargetDoorState closing');
-
     } else if (value === this.platform.Characteristic.TargetDoorState.OPEN) {
       const openCallback = () => {
-        this.service.setCharacteristic(this.platform.Characteristic.CurrentDoorState, this.platform.Characteristic.CurrentDoorState.OPEN);
+        this.service.setCharacteristic(
+          this.platform.Characteristic.CurrentDoorState,
+          this.platform.Characteristic.CurrentDoorState.OPEN,
+        );
         this.platform.log.debug('callback open');
         callback();
         this.platform.log.debug('callback open done');
-
       };
 
       this.open(openCallback);
@@ -162,7 +175,10 @@ export class ChamberlainAccessory {
   getCurrentDoorState(callback: CharacteristicGetCallback) {
     const statusCallback = (status: string) => {
       this.platform.log.debug(`status: ${status}`);
-      this.service.setCharacteristic(this.platform.Characteristic.CurrentDoorState, this.platform.Characteristic.CurrentDoorState.CLOSED);
+      this.service.setCharacteristic(
+        this.platform.Characteristic.CurrentDoorState,
+        this.platform.Characteristic.CurrentDoorState.CLOSED,
+      );
       this.platform.log.debug('callback status');
       callback();
       this.platform.log.debug('callback status done');
